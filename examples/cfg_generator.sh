3b9e08624546fa7c09b8db2b0383c01fbb364aa0
sasile <sasi@iguaz.io>
2017-02-13 17:47:37 +0200

    generator variables

--- examples/cfg_generator.sh
+++ examples/cfg_generator.sh
@@ -1,11 +1,36 @@
 #!/bin/bash
+GET_FILE="blaster_get.cfg"
+PUT_FILE="blaster_put.cfg"
+SERVER="127.0.0.1"
+PORT="8081"
+BUCKET="1"
+
+PUT_DURATION="200s"
+PUT_LOAD_START=1
+PUT_LOAD_END=100
+PUT_LOAD_WORKERS=1
+PUT_COUNT=1000
+PUT_FILES_START=0
+PUT_FILES_COUNT=30000
+
+
+GET_DURATION="200s"
+GET_COUNT=1000
+GET_LOAD_START=1
+GET_LOAD_END=100
+GET_LOAD_WORKERS=10
+GET_FILES_START=0
+GET_FILES_COUNT=30000
+
+
+#generate put workload file
 echo "title =\"Workload put\"
 
 [global]
 block_size = 4096
-Duration = \"160s\"
-server=\"192.168.202.21\"
-port=\"8081\"
+Duration = \"7200s\"
+server=\"$SERVER\"
+port=\"$PORT\"
 TLSMode=false
  [global.StatusCodesAcceptance]
   200 = 100.0
@@ -13,26 +38,28 @@ TLSMode=false
   205 = 100.0
 
 [workloads]
-" > blaster_multi_put.cfg
-for i in {1..100}; do echo "[workloads.$i]
+" > $PUT_FILE
+for (( i=$PUT_LOAD_START; i<=$PUT_LOAD_END; i++ )) do echo "[workloads.$i]
     name=\"PUT-$i\"
-    bucket=\"1\"
+    bucket=\"$BUCKET\"
     file_path=\"$i/test_file\"
-    Duration = \"7200s\"
+    Duration = \"$PUT_DURATION\"
     TYPE=\"PUT\"
-    workers=1
-    count=3000000
-    FileIndex=0
-    FilesCount=3000000" >> blaster_multi_put.cfg; done
+    workers=$PUT_LOAD_WORKERS
+    count=$PUT_COUNT
+    FileIndex=$PUT_FILES_START
+    FilesCount=$PUT_FILES_COUNT" >> $PUT_FILE; done
 
 
+
+#generate get workload file
 echo "title =\"Workload get\"
 
 [global]
 block_size = 4096
-Duration = \"160s\"
-server=\"192.168.202.21\"
-port=\"8081\"
+Duration = \"7200s\"
+server=\"$SERVER\"
+port=\"$PORT\"
 TLSMode=false
  [global.StatusCodesAcceptance]
   200 = 100.0
@@ -40,14 +67,14 @@ TLSMode=false
   205 = 100.0
 
 [workloads]
-" > blaster_multi_get.cfg
-for i in {1..100}; do echo "[workloads.$i]
-    name=\"PUT-$i\"
-    bucket=\"1\"
+" > $GET_FILE
+for (( i=$GET_LOAD_START; i<=$GET_LOAD_END; i++ )) do echo "[workloads.$i]
+    name=\"GET-$i\"
+    bucket=\"$BUCKET\"
     file_path=\"$i/test_file\"
-    Duration = \"300s\"
+    Duration = \"$GET_DURATION\"
     TYPE=\"GET\"
-    workers=10
-    count=300000
-    FileIndex=0
-    FilesCount=3000000" >> blaster_multi_get.cfg; done
+    workers=$GET_LOAD_WORKERS
+    count=$GET_COUNT
+    FileIndex=$GET_FILES_START
+    FilesCount=$GET_FILES_COUNT" >> $GET_FILE; done
