72f050d3ca0c6148cea05ebbb56fa58a2ee82e3e
sasile <sasi@iguaz.io>
2017-06-05 15:52:09 +0300

    fix conflict

--- httpblaster/stream_record_test.go
+++ httpblaster/stream_record_test.go
@@ -1,19 +1,18 @@
 package httpblaster
 
 import (
-	"testing"
 	"github.com/v3io/http_blaster/httpblaster/igz_data"
+	"testing"
 )
 
-func TestStreamRecord(T *testing.T)  {
-	r:= igz_data.NewStreamRecord("testclinet", "testdata", "test_partition_key", 2, false)
+func TestStreamRecord(T *testing.T) {
+	r := igz_data.NewStreamRecord("testclinet", "testdata", "test_partition_key", 2, false)
 	sr := igz_data.NewStreamRecords(r)
 	T.Log(sr.ToJsonString())
 }
 
-
-func TestStreamRecordNoSharded(T *testing.T)  {
-	r:= igz_data.NewStreamRecord("testclinet", "testdata", "test_partition_key", 2, true)
+func TestStreamRecordNoSharded(T *testing.T) {
+	r := igz_data.NewStreamRecord("testclinet", "testdata", "test_partition_key", 2, true)
 	sr := igz_data.NewStreamRecords(r)
 	T.Log(sr.ToJsonString())
 }
